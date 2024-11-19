1b49ec587741adaede4a29225b07f262c856672e
ThiagoC0STA <thiago@lacostacorretora.com.br>
2024-11-18 21:10:43 -0300

    home page changed

--- app/page.jsx
+++ app/page.jsx
@@ -4,30 +4,28 @@ import dynamic from "next/dynamic";
 const ParallaxContainer = dynamic(
   () => import("@/components/common/ParallaxContainer"),
   {
-    ssr: false, 
+    ssr: false, // Disable server-side rendering
   }
 );
 import Hero1 from "@/components/homes/home-1/heros/Hero1";
-
-import { mainDemoOnepage } from "@/data/menu";
+import Header1Multipage from "@/components/headers/Header1Multipage";
+import { menuItemsDark } from "@/data/menu";
 import Footer1 from "@/components/footers/Footer1";
-
 export const metadata = {
   title:
-    "Home 1 Main Demo Onepage Dark || Resonance &mdash; One & Multi Page React Nextjs Creative Template",
+    "Home 1 Main Demo MultiPage Dark || Resonance &mdash; One & Multi Page React Nextjs Creative Template",
   description:
     "Resonance &mdash; One & Multi Page React Nextjs Creative Template",
 };
-export default function Home() {
+export default function Home1MainDemoMultiPageDark() {
   return (
     <>
-      {" "}
-      <div className="theme-main">
-        <div className="dark-mode">
-          {" "}
+      <div className="dark-mode">
+        {" "}
+        <div className="theme-main">
           <div className="page bg-dark-1" id="top">
-            <nav className="main-nav dark dark-mode transparent stick-fixed wow-menubar wch-unset">
-              <Header1 links={mainDemoOnepage} />
+            <nav className="main-nav dark dark-mode transparent stick-fixed wow-menubar">
+              <Header1Multipage links={menuItemsDark} />
             </nav>{" "}
             <main id="main">
               <ParallaxContainer
@@ -40,7 +38,7 @@ export default function Home() {
               >
                 <Hero1 />
               </ParallaxContainer>
-              <Home1 onePage dark={true} />
+              <Home1 dark={true} />
             </main>
             <Footer1 dark />
           </div>{" "}
